030f18775fd4d91bd537f345419affbda77ac957
Jonathan Horowitz <jhorowitz@aod-tech.com>
2019-11-08 20:53:32 -0700

    Node v12 support.

--- verushash.cc
+++ verushash.cc
@@ -1,12 +1,13 @@
-#include <nan.h>
 #include <node.h>
 #include <node_buffer.h>
 #include <v8.h>
+
 #include <stdint.h>
 #include <vector>
 
 #include "crypto/verus_hash.h"
 
+using namespace node;
 using namespace v8;
 
 CVerusHash* vh;
@@ -15,10 +16,8 @@ CVerusHashV2* vh2b1;
 
 bool initialized = false;
 
-void initialize()
-{
-    if (!initialized)
-    {
+void initialize() {
+    if (!initialized) {
         CVerusHash::init();
         CVerusHashV2::init();
     }
@@ -30,71 +29,58 @@ void initialize()
     initialized = true;
 }
 
-void verusInit(const v8::FunctionCallbackInfo<Value>& args) {
+void verusInit(const FunctionCallbackInfo<Value>& args) {
     initialize();
     args.GetReturnValue().Set(args.This());
 }
 
-void verusHash(const v8::FunctionCallbackInfo<Value>& args) {
-    Isolate* isolate = Isolate::GetCurrent();
-    HandleScope scope(isolate);
+void verusHash(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+
     if (args.Length() < 1) {
-        isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments"))
-        );
-        return;
+        do {
+            isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Wrong number of arguments")));
+            return;
+        } while (0);
     }
-    MaybeLocal<Object> maybeBuffer = Nan::To<v8::Object>(args[0]);
-    Local<Object> buffer;
-    if (maybeBuffer.ToLocal(&buffer) != true) {
-        isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Invalid buffer objects."))
-        );
-        return;
-    }
-    if(!node::Buffer::HasInstance(buffer)) {
-        isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Invalid buffer objects."))
-        );
-        return;
+    Local<Object> buffer = args[0]->ToObject(isolate);
+    if(!Buffer::HasInstance(buffer)) {
+        do {
+            isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Invalid buffer objects.")));
+            return;
+        } while (0);
     }
 
-    const char *buff = node::Buffer::Data(buffer);
+    const char *buff = Buffer::Data(buffer);
 
     char *result = new char[32];
     
     if (initialized == false) {
         initialize();
     }
-    verus_hash(result, buff, node::Buffer::Length(buffer));
-    args.GetReturnValue().Set(Nan::NewBuffer(result, 32).ToLocalChecked());
+    verus_hash(result, buff, Buffer::Length(buffer));
+    
+    args.GetReturnValue().Set(Buffer::Copy(isolate, result, 32).ToLocalChecked());
 }
 
-void verusHashV2(const v8::FunctionCallbackInfo<Value>& args) {
-    Isolate* isolate = Isolate::GetCurrent();
-    HandleScope scope(isolate);
+void verusHashV2(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+
     if (args.Length() < 1) {
-        isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments"))
-        );
-        return;
+        do {
+            isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Wrong number of arguments")));
+            return;
+        } while (0);
     }
-    MaybeLocal<Object> maybeBuffer = Nan::To<v8::Object>(args[0]);
-    Local<Object> buffer;    
-    if (maybeBuffer.ToLocal(&buffer) != true) {
-        isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Invalid buffer objects."))
-        );
-        return;
-    }
-    if(!node::Buffer::HasInstance(buffer)) {
-        isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Invalid buffer objects."))
-        );
-        return;
+    Local<Object> buffer = args[0]->ToObject(isolate);
+    if(!Buffer::HasInstance(buffer)) {
+        do {
+            isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Invalid buffer objects.")));
+            return;
+        } while (0);
     }
 
-    const char *buff = node::Buffer::Data(buffer);
+    const char *buff = Buffer::Data(buffer);
 
     char *result = new char[32];
     
@@ -103,36 +89,29 @@ void verusHashV2(const v8::FunctionCallbackInfo<Value>& args) {
     }
 
     vh2->Reset();
-    vh2->Write((const unsigned char *)buff, node::Buffer::Length(buffer));
+    vh2->Write((const unsigned char *)buff, Buffer::Length(buffer));
     vh2->Finalize((unsigned char *)result);
-    args.GetReturnValue().Set(Nan::NewBuffer(result, 32).ToLocalChecked());
+    args.GetReturnValue().Set(Buffer::Copy(isolate, result, 32).ToLocalChecked());
 }
 
-void verusHashV2b(const v8::FunctionCallbackInfo<Value>& args) {
-    Isolate* isolate = Isolate::GetCurrent();
-    HandleScope scope(isolate);
+void verusHashV2b(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+
     if (args.Length() < 1) {
-        isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments"))
-        );
-        return;
+        do {
+            isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Wrong number of arguments")));
+            return;
+        } while (0);
     }
-    MaybeLocal<Object> maybeBuffer = Nan::To<v8::Object>(args[0]);
-    Local<Object> buffer;    
-    if (maybeBuffer.ToLocal(&buffer) != true) {
-        isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Invalid buffer objects."))
-        );
-        return;
-    }
-    if(!node::Buffer::HasInstance(buffer)) {
-        isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Invalid buffer objects."))
-        );
-        return;
+    Local<Object> buffer = args[0]->ToObject(isolate);
+    if(!Buffer::HasInstance(buffer)) {
+        do {
+            isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Invalid buffer objects.")));
+            return;
+        } while (0);
     }
 
-    const char *buff = node::Buffer::Data(buffer);
+    const char *buff = Buffer::Data(buffer);
 
     char *result = new char[32];
     
@@ -141,50 +120,44 @@ void verusHashV2b(const v8::FunctionCallbackInfo<Value>& args) {
     }
 
     vh2->Reset();
-    vh2->Write((const unsigned char *)buff, node::Buffer::Length(buffer));
+    vh2->Write((const unsigned char *)buff, Buffer::Length(buffer));
     vh2->Finalize2b((unsigned char *)result);
-    args.GetReturnValue().Set(Nan::NewBuffer(result, 32).ToLocalChecked());
+    args.GetReturnValue().Set(Buffer::Copy(isolate, result, 32).ToLocalChecked());
 }
 
-void verusHashV2b1(const v8::FunctionCallbackInfo<Value>& args) {
-    Isolate* isolate = Isolate::GetCurrent();
-    HandleScope scope(isolate);
+void verusHashV2b1(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+
     if (args.Length() < 1) {
-        isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments"))
-        );
-        return;
+        do {
+            isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Wrong number of arguments")));
+            return;
+        } while (0);
     }
-    MaybeLocal<Object> maybeBuffer = Nan::To<v8::Object>(args[0]);
-    Local<Object> buffer;    
-    if (maybeBuffer.ToLocal(&buffer) != true) {
-        isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Invalid buffer objects."))
-        );
-        return;
-    }
-    if(!node::Buffer::HasInstance(buffer)) {
-        isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Invalid buffer objects."))
-        );
-        return;
+    Local<Object> buffer = args[0]->ToObject(isolate);
+    if(!Buffer::HasInstance(buffer)) {
+        do {
+            isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Invalid buffer objects.")));
+            return;
+        } while (0);
     }
 
-    const char *buff = node::Buffer::Data(buffer);
+    const char *buff = Buffer::Data(buffer);
 
     char *result = new char[32];
     
     if (initialized == false) {
         initialize();
     }
+
 
     vh2b1->Reset();
-    vh2b1->Write((const unsigned char *)buff, node::Buffer::Length(buffer));
+    vh2b1->Write((const unsigned char *)buff, Buffer::Length(buffer));
     vh2b1->Finalize2b((unsigned char *)result);
-    args.GetReturnValue().Set(Nan::NewBuffer(result, 32).ToLocalChecked());
+    args.GetReturnValue().Set(Buffer::Copy(isolate, result, 32).ToLocalChecked());
 }
 
-void Init(Handle<Object> exports) {
+void init(Local<Object> exports) {
   NODE_SET_METHOD(exports, "init", verusInit);
 
   NODE_SET_METHOD(exports, "hash", verusHash);          //VerusHash V1
@@ -193,4 +166,4 @@ void Init(Handle<Object> exports) {
   NODE_SET_METHOD(exports, "hash2b1", verusHashV2b1);   //VerusHash V2B1
 }
 
-NODE_MODULE(verushash, Init)
+NODE_MODULE(verushash, init)
